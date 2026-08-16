c2ad4d1f6e4762a2e3f3367127404496ff3b0606
Praveen1110214 <praveen1110214@gmail.com>
2026-08-16 14:40:45 +0530

    leetcode solution for 2029

---
+++ src/Leetcode2029/Solution.java
@@ -0,0 +1,29 @@
+package Leetcode2029;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] stones = {1, 2};
+        System.out.println(new Solution().stoneGameIX(stones));
+    }
+    public boolean stoneGameIX(int[] stones) {
+        int[] countByRemainder = new int[3];
+        for (int stone : stones) {
+            countByRemainder[stone % 3]++;
+        }
+        int[] scenario1 = {countByRemainder[0], countByRemainder[1], countByRemainder[2]};
+        int[] scenario2 = {countByRemainder[0], countByRemainder[2], countByRemainder[1]};
+        return check(scenario1) || check(scenario2);
+    }
+    private boolean check(int[] remainderCounts) {
+        remainderCounts[1]--;
+        if (remainderCounts[1] < 0) {
+            return false;
+        }
+        int totalTurns = 1 + Math.min(remainderCounts[1], remainderCounts[2]) * 2 + remainderCounts[0];
+        if (remainderCounts[1] > remainderCounts[2]) {
+            remainderCounts[1]--;
+            totalTurns++;
+        }
+        return totalTurns % 2 == 1 && remainderCounts[1] != remainderCounts[2];
+    }
+}
